579143e814ad22b2d3c0c6b0a3bae45d3ab74f62
Lewis McNeill <lewisfmcn@yahoo.co.uk>
2017-02-20 18:10:06 +0000

    updated connection method

--- Application/php/core/connection.php
+++ Application/php/core/connection.php
@@ -1,13 +1,15 @@
 <?php
 
-$link = mysqli_connect('localhost', 'root','kandersteg');
+$link = @mysqli_connect('localhost', 'root','kandersteg');
 if (!$link) {
     $link = mysqli_connect('localhost', 'root','');
     if(!$link) {
         die('Could not connect to MySQL: ' . mysqli_connect_error());
     }
-} 
+}
 
 mysqli_select_db($link,"lab-marker"); //Selects the Database
-session_start();
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
