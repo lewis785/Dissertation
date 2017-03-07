057bbf690ab8d1b6e0b5e766d41eb3f0add2a504
Lewis <lewisfmcn@yahoo.co.uk>
2017-03-07 03:27:16 +0000

    Started Converting Student

--- Application/php/labs/LabStudents.php
+++ Application/php/labs/LabStudents.php
@@ -8,27 +8,32 @@
  */
 
 require_once "Lab.php";
+require_once(dirname(__FILE__) . "/../students/Student.php");
+require_once(dirname(__FILE__) . "/../marking/Marking.php");
 
 class LabStudents extends Lab
 {
     private $lab;
     private $type;
+    private $student;
+    private $marking;
 
     function __construct()
     {
         $this->lab = isset($_POST["lab"]) ? $_POST["lab"] : null;
         $this->type = isset($_POST["type"]) ? $_POST["type"] : null;
+
+        $this->student = new Student();
+        $this->marking = new Marking();
     }
 
 
     public function get_students_buttons()
     {
-
         $con = new ConnectDB();
 
-        if ($this->can_mark_course($_SESSION["MARKING_COURSE"]))
-        {
-            if($this->type === "next")
+        if ($this->can_mark_course($_SESSION["MARKING_COURSE"])) {
+            if ($this->type === "next")
                 $_SESSION["MARKING_LAB"] = $this->lab;
             else {
                 $_SESSION["MARKING_STUDENT"] = "";
@@ -44,31 +49,30 @@ class LabStudents extends Lab
 
                 $buttonType = $this->button_style($con->link, $student[2], $labID, $this->lab, $courseName);
                 $buttons .= "<div class='col-md-6 col-md-offset-3 col-sm-12'>
-                      <button class='".$buttonType."' id='btn-student' onclick='display_schema_for(\"" . $student[2] . "\")'>" . $student[0] ." ". $student[1] . "</button>
+                      <button class='" . $buttonType . "' id='btn-student' onclick='display_schema_for(\"" . $student[2] . "\")'>" . $student[0] . " " . $student[1] . "</button>
                      </div>";
             }
             $output = json_encode(array('successful' => true, 'buttons' => $buttons));
-        }
-        else
-            $output = json_encode(array('successful'=>false));
+        } else
+            $output = json_encode(array('successful' => false));
 
         mysqli_close($con->link);
         return $output;
     }
 
 
-    private function button_style($link, $matric,$qID, $labName, $courseName)
+    private function button_style($link,$matric, $qID, $labName, $courseName)
     {
 
-//        if(already_marked($link, get_studentID($link,$matric), $qID)) {
-//            if(has_full_marks($matric,$courseName, $labName))
-//                $style = "btn btn-success";
-//            elseif (has_no_marks($matric,$courseName, $labName))
-//                $style = "btn btn-danger";
-//            else
-//                $style = "btn btn-warning";
-//        }
-//        else
+        if ($this->marking->already_marked($this->student->get_studentID($matric), $qID)) {
+            if ($this->student->has_full_marks($link, $matric, $courseName, $labName))
+                $style = "btn btn-success";
+            elseif ($this->student->has_no_marks($link, $matric, $courseName, $labName))
+                $style = "btn btn-danger";
+            else
+                $style = "btn btn-warning";
+
+        } else
             $style = "btn btn-info";
 
         return $style;
@@ -78,12 +82,11 @@ class LabStudents extends Lab
     public function get_students($course)
     {
         $output = [];
-        if ($this->can_mark_course($course))
-        {
+        if ($this->can_mark_course($course)) {
             $con = new ConnectDB();
 
             $get_students = mysqli_stmt_init($con->link);
-            mysqli_stmt_prepare($get_students, "SELECT d.firstname, d.surname, d.studentID from students_on_courses as soc 
+            mysqli_stmt_prepare($get_students, "SELECT d.firstname, d.surname, d.studentID FROM students_on_courses AS soc 
                                         JOIN user_details AS d ON soc.student = d.detailsId 
                                         JOIN courses AS c ON soc.course = c.courseID 
                                         WHERE c.courseName = ? 
@@ -102,4 +105,4 @@ class LabStudents extends Lab
 
 
 //$student = new LabStudents();
-//echo($student->get_students_buttons("df"));
+//echo($student->get_students_buttons("back"));
--- Application/php/students/Student.php
+++ Application/php/students/Student.php
@@ -8,9 +8,17 @@
  */
 
 require_once (dirname(__FILE__)."/../core/ConnectDB.php");
+require_once (dirname(__FILE__)."/../labs/Lab.php");
+
 
 class Student
 {
+    private $lab;
+
+    function __construct()
+    {
+        $this->lab = new Lab();
+    }
 
     public function get_studentID($student)
     {
@@ -25,6 +33,37 @@ class Student
         $result = mysqli_stmt_get_result($get_studentsID)->fetch_row();
 
         mysqli_close($con->link);
+        return $result[0];
+    }
+
+    public function has_full_marks($link, $student,$courseName, $labName)
+    {
+        $maxMark = $this->lab->lab_total_mark($courseName,$labName);
+        $studentMark = $this->lab_mark_for_student($link, $student, $courseName, $labName);
+        return $studentMark == $maxMark;
+    }
+
+    public function has_no_marks($link, $student,$courseName, $labName)
+    {
+        $studentMark = $this->lab_mark_for_student($link, $student, $courseName, $labName);
+        return $studentMark == 0;
+    }
+
+    public function lab_mark_for_student($link, $matricID, $courseName, $labName)
+    {
+        $labMark = mysqli_stmt_init($link);
+        mysqli_stmt_prepare($labMark, "SELECT sum(mark) FROM lab_answers AS la
+                              JOIN lab_questions AS lq ON la.labQuestionRef = lq.questionID
+                              JOIN labs AS l ON lq.labRef = l.labID
+                              JOIN courses AS c ON l.courseRef = c.courseID
+                              JOIN students_on_courses AS soc ON la.socRef = soc.socID
+                              JOIN user_details AS ud ON soc.student = ud.detailsId 
+                              WHERE ud.studentID = ? AND c.courseName = ? AND l.labName = ?");
+        mysqli_stmt_bind_param($labMark, "sss", $matricID, $courseName, $labName);
+
+        mysqli_stmt_execute($labMark);
+        $result = mysqli_stmt_get_result($labMark)->fetch_row();
+
         return $result[0];
     }
 
